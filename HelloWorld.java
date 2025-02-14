aafb2fa136133c4c5d13336928f1fedd1ef7c536
ranchitos <ranchito62406@gmail.com>
2025-02-14 14:09:51 -0800

    Updated java code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("HELLO GIT");
+        }
     }
 }
